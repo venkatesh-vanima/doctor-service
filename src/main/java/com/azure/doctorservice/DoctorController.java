c2eed0c521c0653b82a8e888b2a3efb319821b2d
venkatesh vanima <venkateshvanima1@gmail.com>
2026-06-07 21:49:43 +0530

    add-new-endpoints

--- src/main/java/com/azure/doctorservice/DoctorController.java
+++ src/main/java/com/azure/doctorservice/DoctorController.java
@@ -1,5 +1,6 @@
 package com.azure.doctorservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,11 @@ import java.util.List;
 public class DoctorController {
 
     private final DoctorService doctorService;
+
+    @Value("${config1}")
+    private  String configValue;
+    @Value("${config2}")
+    private String configValue2;
 
     public DoctorController(DoctorService doctorService) {
         this.doctorService = doctorService;
@@ -38,5 +44,17 @@ public class DoctorController {
     public ResponseEntity<List<DoctorDto>> getDoctors() {
         List<DoctorDto> doctorDto = doctorService.findAllDoctors();
         return new ResponseEntity<>(doctorDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/secrets")
+    public ResponseEntity<String> getSecrets() {
+        String secrets = "name: Venkatesh bestfriend: ishira";
+        return new ResponseEntity<>(secrets, HttpStatus.OK);
+    }
+
+    @GetMapping("/config")
+    public ResponseEntity<String> getConfig() {
+        String value = configValue + " " + configValue2;
+        return new ResponseEntity<>(value, HttpStatus.OK);
     }
 }
